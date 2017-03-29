6393a74e427a55b3934493e9d8c4e2608f88cc5e
deven1 <deven@sloan.mit.edu>
2017-03-29 13:34:10 -0400

    3rd commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let express = require('./lib/express');
 let app = express;
 
 app.listen(3000, 'localhost')
-app.get('/', function(res){
-  console.log("it worked")
-})
 
+
+app.get('/', (req, res) => {
+  //console.log(req);
+  res.end('Hi world!\n');
+});
