c62983a4bd4e89106224a9731bc48e11ced4073f
William Whitworth <william10charles@gmail.com>
2017-03-29 12:04:41 -0600

    param beginnings

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("./express");
 
 // var app = express();
 
-express.get('/puppies', (req, res) => {
-  res.end('Puppies!!!\n');
+express.get("/puppies", (req, res) => {
+  res.end("Puppies!!!\n");
 });
 
-
-
+express.get("/puppies/:foo", (req, res) => {
+  res.end("Cats\n");
+});
