f6cf274ceb3a5e2a53b8da932a0c781efb89e461
William Whitworth <william10charles@gmail.com>
2017-03-29 15:00:46 -0600

    working on param selection

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const app = require("./express");
-
+const puppies = require("./lib/puppies");
 // var app = express();
 
 var port = 3000;
-var host = 'localhost';
+var host = "localhost";
 
 app.listen(port, host, () => {
-	console.log("Still working?")
+  console.log("Still working?");
 });
 
 app.get("/puppies", (req, res) => {
-  res.end("Puppies!!!\n");
+  res.end("Puppies!!!");
 });
 
-app.get("/puppies/:foo", (req, res) => {
-  res.end("Cats\n");
-});
-
-app.get('/puppies/:breed', function(req, res) {
-  const breed = findBreed(req.params.breed)
+app.get("/puppies/:breed", function(req, res) {
+  const breed = puppies[req.params.breed];
   res.end(`${breed}`);
+});
+
+app.get("/", function(req, res) {
+  const name = people[req.params.name];
+  res.end(`${name}`);
 });
--- express.js
+++ express.js
@@ -1,19 +1,19 @@
-const http = require('http');
-var router = require('./lib/router');
+const http = require("http");
+var router = require("./lib/router");
 // var routes = require('./routes');
 
-var express = {}
+var express = {};
 
 // var server = http.createServer(router.handle);
 var server = http.createServer(router.handle);
 
 function listen(port, host, callback) {
-	server.listen(port, host, callback);
-};
+  server.listen(port, host, callback);
+}
 
 function get(path, callback) {
-	router.routes.get[path] = callback;
-	console.log(router.routes);
-};
+  router.routes.get[path] = callback;
+  console.log(router.routes);
+}
 
 module.exports = { get, listen };
