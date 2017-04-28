8d4fa6c243feac37e79a652013472ec48a73e263
rabGIT <rabGIT@users.noreply.github.com>
2017-04-28 16:19:44 -0700

    additional HTTP method handling

--- app.js
+++ app.js
@@ -7,17 +7,22 @@ var app = express();
 
 // 3. Create routes via the app object
 app.get('/', (req, res) => {
-//  console.log(req);
-  res.end('Hi world!\n');
+  console.log(req);
+  res.end('GET: Hi world!\n');
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.end(`POST: Hi world!\n ${req.body}\n`);
 });
 
 app.get('/foo/:bar/fiz/:baz/', (req, res) => {
-//  console.log(req);
+  console.log(req);
   res.end(`Hi /foo/:bar/fiz/:baz/! bar ${req.params.bar} baz ${req.params.baz}\n`);
 });
 
 app.get('/foo/:bar/', (req, res) => {
-//  console.log(req);
+  console.log(req);
   res.end(`Hi /foo/:bar/! bar ${req.params.bar}\n`);
 });
 
