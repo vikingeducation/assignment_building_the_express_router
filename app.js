7d5bec65af2c842b008bbf9120deec01887527cb
rabGIT <rabGIT@users.noreply.github.com>
2017-04-28 16:09:37 -0700

    parameter routing w/ regex

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ var app = express();
 
 // 3. Create routes via the app object
 app.get('/', (req, res) => {
-  console.log(req);
+//  console.log(req);
   res.end('Hi world!\n');
 });
+
+app.get('/foo/:bar/fiz/:baz/', (req, res) => {
+//  console.log(req);
+  res.end(`Hi /foo/:bar/fiz/:baz/! bar ${req.params.bar} baz ${req.params.baz}\n`);
+});
+
+app.get('/foo/:bar/', (req, res) => {
+//  console.log(req);
+  res.end(`Hi /foo/:bar/! bar ${req.params.bar}\n`);
+});
+
+
 
 // 4. Start up a server with app.listen
 var port = process.env.PORT || 4000;
