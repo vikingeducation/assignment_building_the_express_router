78fa222da57b338301b7048617e67eff29fba2db
Christian Florez <florez.christian@gmail.com>
2017-05-17 18:56:21 -0400

    add parameterized routing

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.get('/foo', (req, res) => {
   res.end('You accessed foo!!\n');
 });
 
+app.get('/bar', (req, res) => {
+  res.end('You accessed bar!!\n');
+});
+
+app.get('/foo/:bar/biz/:baz', (req, res) => {
+  let params = JSON.stringify(req.params);
+  res.end(`You accessed /foo/:bar/biz/:baz and your params are ${ params }!!\n`);
+});
+
+app.get('/foo/:sombrero', (req, res) => {
+  let params = JSON.stringify(req.params);
+  res.end(`You accessed foo/sombrero and your params are ${ params }!!\n`);
+});
+
 app.listen(port, () => {
   console.log(`Listening on ${ port }`);
 });
