fade5479be32f844ba7a2635d378b56b96e8135b
Christian Florez <florez.christian@gmail.com>
2017-05-17 19:06:24 -0400

    implement additional http methods (put, patch, and delete)

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/foo', (req, res) => {
   res.end('You accessed foo!!\n');
 });
 
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.end('You accessed the root url with post!\n');
+});
+
 app.get('/bar', (req, res) => {
   res.end('You accessed bar!!\n');
 });
@@ -22,6 +27,18 @@ app.get('/foo/:bar/biz/:baz', (req, res) => {
 app.get('/foo/:sombrero', (req, res) => {
   let params = JSON.stringify(req.params);
   res.end(`You accessed foo/sombrero and your params are ${ params }!!\n`);
+});
+
+app.put('/testput', (req, res) => {
+  res.end('You successfully accessed /testput using a put method!');
+});
+
+app.patch('/testpatch', (req, res) => {
+  res.end('You successfully accessed /testpatch using a patch method!');
+});
+
+app.delete('/testdelete', (req, res) => {
+  res.end('You successfully accessed /testdelete using a delete method!');
 });
 
 app.listen(port, () => {
