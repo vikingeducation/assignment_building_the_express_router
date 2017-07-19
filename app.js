ffa93f572cfa9b0575bd2bfc67ee8a3912def364
Eric Glover <etgt28@mail.missouri.edu>
2017-07-19 12:38:31 -0500

    parameters

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ app.listen(port, () => {
   console.log("Listening on port " + port);
 });
 
-app.get("/whee", (req, res) => {
-	res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-	res.end("I sent a thing?");
-})
-
+//bug : no longer will serve just /whee correctly
 app.get("/whee/:id", (req, res) => {
-	res.end("ID: " + req.params.id);
-})
+  res.end("ID: " + req.params.id);
+});
+
+app.get("/whee", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("I sent a thing?");
+});
