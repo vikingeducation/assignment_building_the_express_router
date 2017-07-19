3d4aa3ffab8790e1cf9a69137764efddb149b842
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-19 15:05:36 -0400

    pomo

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let express = require("./lib/express.js");
 const app = express();
 console.log(app);
 
-// app.get("/", (req, res) => {
-//   console.log(req);
-//   res.end("Hi world!\n");
-// });
+app.get("/", (req, res) => {
+   console.log(req);
+   res.end("Hi world!\n");
+});
 
 var port = process.env.PORT || 3000;
 var host = "localhost";
