4695fb130a98133c0894bfbce616a49fb9180a6e
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-19 21:24:21 +0000

    working on pattern match

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ let express = require("./lib/express.js");
 
 const app = express();
 var port = process.env.PORT || 3000;
-var host = 'localhost' || "0.0.0.0";
+var host = "0.0.0.0";
 
 app.get("/", (req, res) => {
   res.end("Hi world!\n");
 });
 
-//app.get("/favicon.ico", function(req, res) {
-//  console.log("stupid favicon");
-//});
+app.get("/foo", function(req, res) {
+  res.end("this is foo");
+});
+
+app.get("/foo/:bar", function(req, res) {
+  res.end();
+});
 
 app.listen(port, host, () => {
   console.log(`Listening at: http://${host}:${port}`);
--- parser.js
+++ parser.js
@@ -1,3 +1,13 @@
-let parser = (path) => {
-  path.split('/')
-}
+let pattern = {};
+
+let parser = path => {
+  newPath = path.split("/");
+  newPath = newPath.slice(1);
+  console.log(newPath);
+  newPath.forEach(word => {
+    pattern[word] = word;
+  });
+  console.log(pattern);
+};
+
+module.exports = parser;
