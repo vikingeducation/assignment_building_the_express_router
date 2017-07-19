78e144760ee1d04c8d4d957620abd394a609f671
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-19 12:07:53 -0400

    finished listen method

--- ourExpress.js
+++ ourExpress.js
@@ -1,23 +1,38 @@
 var httpObj = require("http");
-
+var path = '';
 var app = {
-  var port = process.env.PORT || 4000;
-  var host = "localhost";
-  var StartServer = function(path,callback) {
-//create server
-let server = httpObj.createServer(callback);
-server.listen(port,host, fuction(req,res){});
+  
+  StartServer: function(port, host, callback) {
+		//create server
+		let server = httpObj.createServer(callback);
 
+		server.listen(port, host, () => { 
+			console.log(`Server running at http://${host}:${port}/${path}`);
+		});
 
-//create listener
+    return;
+  },
 
-    return
-  };
-  var Get = function() {
-    return
-  };
+  Get: function(newPath, callback) {
+  	path = newPath;
+    return;
+  }
 };
 
 var GetApp = function() {
   return app;
 };
+
+let port = process.env.PORT || 4000;
+let host = "localhost";
+
+app.StartServer(port, host, (req, res) => {
+	// console.log("yay!!!!!!!!")
+	res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+app.Get('cat', () => {
+	console.log('meow');
+})
