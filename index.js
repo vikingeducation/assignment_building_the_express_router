9d06c4b5093bf5ff7270cadfa8dc8e9cf7679c57
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-19 17:45:04 +0000

    s

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 let express = require("./lib/express.js");
 
-// 2. Create an application with the
-// returned function
 const app = express();
 console.log(app);
 
-//3. Create routes via the app object
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//   console.log(req);
+//   res.end("Hi world!\n");
+// });
 
-  console.log(req);
-  res.end("Hi world!\n");
-});
-
-// 4. Start up a server with app.listen
 var port = process.env.PORT || 3000;
 var host = "localhost";
 
