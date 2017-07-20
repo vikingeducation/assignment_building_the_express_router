bde212a4fff84f2f48a3bb75855715240bcfa29f
Will Timpson <will.timpson@gmail.com>
2017-07-19 20:52:30 -0400

    Refactor parsePaths, moar tests

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const express = require('./lib');
 var app = express();
 
 app.get('/', (req, res) => {
-  console.log('no params...');
+  console.log('no params...\n');
   res.end('Hi world!\n');
 });
 
 app.get('/:name/:pet', (req, res) => {
+  console.log('Two params!\n');
   res.write(JSON.stringify(req.params));
-  console.log('Two params!');
-  res.end('Two params!!!!');
+  res.end('Two params!!!!\n');
 });
 
 app.get('/:name/has/:pet/', (req, res) => {
+  console.log('Two params surrounding a literal!\n');
   res.write(JSON.stringify(req.params));
-  console.log('Two params surrounding a literal!');
-  res.end('Two params surrounding a literal!!!!');
+  res.end('Two params surrounding a literal!!!!\n');
 });
 
 app.get(/^\/[a-zA-Z]{3}\d{3}$/, (req, res) => {
-  console.log('Whoa! A Regex!');
-  res.end('Whoa! A Regex!');
+  console.log('Whoa! A Regex!\n');
+  res.end('Whoa! A Regex!\n');
 });
 
 app.get('/:name', (req, res) => {
+  console.log('One param!\n');
   res.write(JSON.stringify(req.params));
-  console.log('One param!');
-  res.end('One Param!!!!');
+  res.end('One Param!!!!\n');
 });
 
 app.post('/', (req, res) => {
@@ -38,26 +38,25 @@ app.post('/', (req, res) => {
 });
 
 app.put('/', (req, res) => {
-  console.log('We, were put upon.');
+  console.log('We, were put upon.\n');
   console.log(req.body);
   res.write(JSON.stringify(req.body));
   res.end('Put me down!\n');
 });
 
 app.patch('/', (req, res) => {
-  console.log('Patch me in! I have the message!');
+  console.log('Patch me in! I have the message!\n');
   console.log(req.body);
   res.write(JSON.stringify(req.body));
   res.end('Have you applied the patches?\n');
 });
 
 app.delete('/', (req, res) => {
-  console.log('[This object intentionally left blank]');
+  console.log('[This object intentionally left blank]\n');
   console.log(req.body);
   res.write(JSON.stringify(req.body));
   res.end('Deleted, yo.\n');
 });
-
 
 var port = process.env.PORT || 4000;
 var host = 'localhost';
--- requests.sh
+++ requests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Funky paths
+curl http://localhost:4000
+curl http://localhost:4000/jenny
+curl http://localhost:4000/joanne/tiger
+curl http://localhost:4000/suzy/has/elephant
+curl http://localhost:4000/abc123
+
+# Test RESTful methods
 curl --request POST -H 'Content-Type: application/json' -d "{'this is': 'post'}" http://localhost:4000
 curl --request PUT -H 'Content-Type: application/json' -d "{'this is': 'put'}" http://localhost:4000
 curl --request PATCH -H 'Content-Type: application/json' -d "{'this is': 'patch'}" http://localhost:4000
