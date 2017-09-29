95b4796b62f52a5fa6be0e28c3a695ee8b9ddea2
maddiereddy <maddiereddy@gmail.com>
2017-09-29 00:01:52 -0700

    incomplete parsing path patterns

--- app.js
+++ app.js
@@ -7,7 +7,29 @@ const app = express();
 
 // 3. Create routes via the app object
 app.get('/', (req, res) => {
-  res.end('Hello Express!\n');
+  res.end('Hello Express Router!\n');
+});
+
+app.get('/foo/:bar/', (req, res) => {
+  res.end(`Hi /foo/:bar/\n`);
+});
+
+app.get('/foo/:bar/fiz/:baz/', (req, res) => {
+  res.end(`Hi /foo/:bar/fiz/:baz/!\n`);
+});
+
+app.post('/', (req, res) => {
+  let data = req.body;
+
+  // If the content type is JSON
+  // parse the data into a JSON string
+  if (req.headers['content-type'] === 'application/json') {
+    data = JSON.parse(req.body);
+    data = JSON.stringify(data, null, '  ');
+  }
+
+  // Output the POST data
+  res.end(`Data: ${ data }`);
 });
 
 // 4. Start up a server with app.listen
