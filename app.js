06b4c0ae40b1cdaaa2d9f29bc3ee34ce9d902173
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-09-30 18:02:28 +0200

    parameterized routing

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const express = require('./lib/express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(req);
   res.end('Hi world!\n');
+});
+
+app.get('/foo/:bar/', (req, res) => {
+  const params = JSON.stringify(req.params);
+  res.end(`your params are ${params}!\n`);
+});
+
+app.get('/foo/:bar/biz/:baz', (req, res) => {
+  const params = JSON.stringify(req.params);
+  res.end(`You accessed /foo/:bar/biz/:baz and your params are ${params}!\n`);
 });
 
 const port = process.env.PORT || 4000;
