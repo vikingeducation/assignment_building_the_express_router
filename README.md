a4c3edb0eb66218e85b6570ed0de2bbec32b3373
DC <heyyodc-git@yahoo.com>
2017-10-15 22:56:35 -0700

    completed!

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 assignment_building_the_express_router
 ======================================
-
 Build the core features of the Express router
 
 ==
 
-by Dennis C
+Synopsis
+-------
+To increase the depth of knowledge of the Express router I implemented its core features in this assignment.
 
+Usage
+-----
+```
+node index.js
+```
+
+Highlights 
+-----
+- http methods: get, post, put, delete, patch
+- working with http request / response objects
+- JSON file manipulation
+- regex
+- testing with curl
+- promises
+
+
+##### by Dennis C
--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const express = require('./lib/express');
 const app = express();
 
 // 3. Create routes
+//test with: localhost:4000
 app.get('/', (req, res) => {
   res.end('GET request to / route\n');
 });
 
+//test with: localhost:4000/foo/1
 app.get('/foo/:bar', (req,res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
@@ -18,6 +20,7 @@ app.get('/foo/:bar', (req,res) => {
 	res.end(`:bar = ${req.params.bar}`);
 });
 
+//test with: localhost:4000/foo/1/fiz/2
 app.get('/foo/:bar/fiz/:baz', (req,res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
@@ -25,7 +28,16 @@ app.get('/foo/:bar/fiz/:baz', (req,res) => {
 	res.end(`:bar = ${req.params.bar}<br>:baz = ${req.params.baz}`);
 });
 
-//test POST with curl on windows command-line
+//test with: localhost:4000/username/dc/password/s3cr3t
+app.get('/username/:username/password/:password', (req,res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.write('path = /username/:username/password/:password<br>');	
+	res.end(`:username = ${req.params.username}<br>:password = ${req.params.password}`);
+});
+
+//test with: 1) curl -d "foo=bar" http://localhost:4000
+//           2) curl -H "Content-Type: application/json" -d '{ "foo": "bar" }' http://localhost:4000
 app.post('/', (req, res) => {
   let data = req.body;
 
@@ -33,8 +45,43 @@ app.post('/', (req, res) => {
     data = JSON.parse(req.body);
     data = JSON.stringify(data, null, 2);
   }
-
   res.end(`\nDC's POST data: ${ data }`);
+});
+
+//test with: 1) curl -X PUT -d "foo=bar" http://localhost:4000
+//           2) curl -X PUT -H "Content-Type: application/json" -d '{ "foo": "bar" }' http://localhost:4000
+app.put('/', (req, res) => {
+  let data = req.body;
+
+  if (req.headers['content-type'] === 'application/json') {
+    data = JSON.parse(req.body);
+    data = JSON.stringify(data, null, 2);
+  }
+  res.end(`\nDC's PUT data: ${ data }`);
+});
+
+//test with: 1) curl -X PATCH -d "foo=bar" http://localhost:4000
+//           2) curl -X PATCH -H "Content-Type: application/json" -d '{ "foo": "bar" }' http://localhost:4000
+app.patch('/', (req, res) => {
+  let data = req.body;
+
+  if (req.headers['content-type'] === 'application/json') {
+    data = JSON.parse(req.body);
+    data = JSON.stringify(data, null, 2);
+  }
+  res.end(`\nDC's PATCH data: ${ data }`);
+});
+
+//test with: 1) curl -X DELETE -d "foo=bar" http://localhost:4000
+//           2) curl -X DELETE -H "Content-Type: application/json" -d '{ "foo": "bar" }' http://localhost:4000
+app.delete('/', (req, res) => {
+  let data = req.body;
+
+  if (req.headers['content-type'] === 'application/json') {
+    data = JSON.parse(req.body);
+    data = JSON.stringify(data, null, 2);
+  }
+  res.end(`\nDC's DELETE data: ${ data }`);
 });
 
 // 4. Start up a server, and listener
