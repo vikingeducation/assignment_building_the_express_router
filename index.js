657f50f3e6ab9824d1d40f8fdbacea025111ec37
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-15 10:38:11 -0600

    tomatoes

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+var router = require('./lib/index.js');
+
+var port = process.env.PORT || process.argv[2] || 3000;
+var host = 'localhost';
+
+var server = http.createServer(router.handle);
+
+server.listen(port, host, () => {
+	console.log("Hello World!");
+	});
