34a0b60b78852ef6679a3c769130d718c4189194
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-15 11:35:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("./lib/express.js");
+
+var app = express;
+app.listen(3000, "localhost", () => {
+  console.log("app is listening");
+});
+
+app.get("/", (req, res) => {
+  res.end("Hello World!");
+});
