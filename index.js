1b57a1e0729fe9ce552681a8300cb9ddc1402896
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-15 17:20:07 -0500

    working on post

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/foo/:bar", (req, res) => {
+  let keys = Object.keys(req.params);
+  keys.forEach(key => {
+    res.write(req.params[key]);
+  });
   res.end("foo bar");
   //we want foo bar to show up on /foo/johndoe
 });
 
-/*
-app.get("/", (req, res) => {
-  res.end("Hello World!");
+app.post("/post/:form", (req, res) => {
+  res.end("form posted");
 });
-*/
