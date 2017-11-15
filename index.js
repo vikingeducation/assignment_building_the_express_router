b7bc7bf43491649a72d97479f2142ab06095dcf9
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-15 12:33:53 -0500

    working on paramterized routes

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.listen(3000, "localhost", () => {
   console.log("app is listening");
 });
 
+app.get("/", (req, res) => {
+  res.end("get method was fired");
+});
+
+app.get("/foo/:bar", (req, res) => {
+  res.end("foo bar");
+});
+
 /*
 app.get("/", (req, res) => {
   res.end("Hello World!");
