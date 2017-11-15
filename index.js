836d05b8a41436d3696f12ff9a434e3d670a9b8d
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-15 17:10:32 -0600

    post echo

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.get('/:name', (req, res)=>{
 		console.log('works');
 		res.end('It works!\n');
 		});
+app.post('/', (req, res) =>{
+	let body = '';
+	req.on('data', (data) => {
+		body += data;
+		});
+	req.on('end', () =>{
+		res.end(body);
+		});
+});
 // 4. Start up a server with app.listen
 var port = process.env.PORT || 4000;
 var host = 'localhost';
