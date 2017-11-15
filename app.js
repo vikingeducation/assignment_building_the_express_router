ebe2d7f1a069b6e92fdf13c7c40e43b74dddb184
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-15 16:29:59 -0700

    working on parser

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ let app = express();
 let port = process.env.PORT || 3000;
 let host = "localhost";
 
-
 // Use the router to register callbacks
 // for paths and HTTP verbs
 app.get('/', (req, res) => {
   console.log("inside get");
   res.end('Hello GET!');
+});
+
+app.get('/:user', (req, res) => {
+  res.end("userTest");
 });
 
 app.post('/', (req, res) => {
@@ -22,6 +25,3 @@ app.post('/', (req, res) => {
 app.listen(port, host, () => {
   console.log(`server running at http://${host}:${port}/`);
 });
-
-
-
