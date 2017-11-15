1988ea02500bc0193c61360d38a07d49ef483347
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-15 15:16:42 -0600

    quick fix

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ let app = express();
 
 // 3. Create routes via the app object
 app.get('/', (req, res) => {
-  console.log(req);
-  res.end('Hi world!\n');
-});
-
+		console.log(req);
+		res.end('Hi world!\n');
+		});
+app.get('/:name', (req, res)=>{
+		console.log('works');
+		res.end('It works!\n');
+		});
 // 4. Start up a server with app.listen
 var port = process.env.PORT || 4000;
 var host = 'localhost';
 
 app.listen(port, () => {
-  console.log(`Listening at: http://${ host }:${ port }`);
-});
+		console.log(`Listening at: http://${ host }:${ port }`);
+		});
